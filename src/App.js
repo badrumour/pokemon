ab7fd44da39acea4629ac3329d5acec06c3c1135
badrumour <sanjeetkshirasagar10@gmail.com>
2023-04-29 15:07:07 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Maine from "./components/Maine";
+import "./components/style.css";
+function App() {
+  return (
+    <>
+      <Maine />
+    </>
+  );
+}
+
+export default App;
